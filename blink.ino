a9ad209e702638a725a0dc865bdcd029904557d0
dizhipeng <dirischlecht@outlook.com>
2016-02-12 16:41:11 +0800

    add buttom support

--- blink.ino
+++ blink.ino
@@ -8,11 +8,13 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
+int button = 9;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);     
+  pinMode(button, INPUT);     
 }
 
 // the loop routine runs over and over again forever:
