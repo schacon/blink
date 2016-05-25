c799a9153684c508a2ea30c325d78e6d9b078595
whbing <whbing@whbing.cn>
2016-05-25 12:26:00 +0800

    my first github test

--- blink.ino
+++ blink.ino
@@ -22,3 +22,4 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
 }
+//this is whbing@whing.cn new addition
