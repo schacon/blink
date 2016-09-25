f662a924fdd172391900764f105fe497520e8647
mali-bmc <mahmoud_ali@bmc.com>
2016-09-25 09:55:35 -0400

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -5,4 +5,6 @@ args=$1;shift
 GIT_HASH=$1;
 echo $GIT_HASH
 
-echo $build.vcs.number
+GITHUB_TOKEN="c04f75a2016bd1d8f11da49a1840ff1c2582bdf9";
+
+curl -s -u ${GITHUB_TOKEN}:x-oauth-basic https://api.github.com/repos/mali-bmc/blink/commits/${GIT_HASH}/statuses | jq --raw-output  '.
