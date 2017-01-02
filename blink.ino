269aaf7cab86ea97dc2b3e50ac95e69516db1995
Nickrader <nick.rader@yahoo.com>
2017-01-02 16:10:22 -0700

    Now that I think about it, 4 is the best of both worlds.

--- blink.ino
+++ blink.ino
@@ -18,7 +18,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(3000);               // wait for a second
+  delay(4000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(3000);               // wait for a second
+  delay(4000);               // wait for a second
 }
