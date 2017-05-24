646bc3c9925b0e3fed99145e8f367e430664ba73
injin13.kim <injin13.kim@samsung.com>
2017-05-24 12:27:44 +0900

    delete comment

--- blink.ino
+++ blink.ino
@@ -15,7 +15,7 @@ void setup() {
   pinMode(led, OUTPUT);     
 }
 
-// the loop routine runs over and over again forever:
+// loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
