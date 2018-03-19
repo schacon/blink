392195a4394626a44f3af834898a20da215b1db8
Dina Tamás Pál <rm9wia@INF.ELTE.HU>
2018-03-19 15:50:16 +0100

    Not so pretty setup routine

--- blink.ino
+++ blink.ino
@@ -9,7 +9,7 @@
 // give it a name:
 int led = 13;
 
-// This is a really nice setup routine
+// This setup routine is not bad
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
