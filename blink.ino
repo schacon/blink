f958dcc845eb34f743feda41c54ac0248178072a
directx90 <jianglxu@gmail.com>
2018-04-29 17:50:16 +0800

    add new line

--- blink.ino
+++ blink.ino
@@ -21,4 +21,5 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
+  delay(2000);               // wait for a second
 }
