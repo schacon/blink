90ced8ec5e9c57c3e3e539fa6e6c0b0799dc1281
Mahmood Younis <younism456@gmail.com>
2018-12-10 21:13:58 +0200

    three seconds is better

--- blink.ino
+++ blink.ino
@@ -22,3 +22,7 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
 }
+sfsdfafsadkjfbshdk
+alfnhaldfndfkjabsnkj
+sldijfsdlingsdfkj
+aldnjvsilf
