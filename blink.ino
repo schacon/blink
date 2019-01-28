15fe43007182b8ee588ea5623a18e4b6cc6d3f3e
Majid Ghafouri <seyedmajid.ghafouri@tradeshift.com>
2019-01-28 15:17:04 +0100

    The correct is 13

--- blink.ino
+++ blink.ino
@@ -7,7 +7,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 14;
+int led = 13;
 
 // the setup routine runs once when you press reset:
 void setup() {                
