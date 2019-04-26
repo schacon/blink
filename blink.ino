266f2c00db01a8c403a9f4a5bf017d5d9ffe7d40
AlpsRunner <43140447+AlpsRunner@users.noreply.github.com>
2019-04-27 00:21:32 +0500

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -21,4 +21,6 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
+  digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
+  delay(3000);               // wait for a second
 }
