2a716ddd4d64087f8e81c837144628b12602763f
simonyao <simonyao996@gmail.com>
2019-07-01 11:56:31 +0800

    three seconds is better

--- blink.ino
+++ blink.ino
@@ -7,7 +7,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
+int led = 12;
 
 // the setup routine runs once when you press reset:
 void setup() {                
