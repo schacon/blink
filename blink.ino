f0c36ecbcd918735a324457797dd117e10b4a6ab
Thomas Wiatowski <twiatowski@gmail.com>
2020-02-18 17:19:47 +0100

    corrected for super fast blink

--- blink.ino
+++ blink.ino
@@ -18,7 +18,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
+  delay(10);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
+  delay(10);               // wait for a second
 }
