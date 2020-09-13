f5578259f2cbc50440849208fd903be82512586a
jazzilll <35569538+jazzilll@users.noreply.github.com>
2020-09-14 03:46:59 +0800

    Added a new Comment

--- blink.ino
+++ blink.ino
@@ -7,6 +7,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
+// hurriedly added this comment to test the pull request
 int led = 13;
 
 // the setup routine runs once when you press reset:
