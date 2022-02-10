9d4d55742a2da029c07d5cbcbba533d3e57985d7
SALAVOZ <salik.abdulin@mail.ru>
2022-02-10 17:40:47 +0400

    fixed all

--- blink.ino
+++ blink.ino
@@ -7,18 +7,18 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:fixing threatsfixing threatsfixing threatsfixing threats
-int led = 13;
+int led = fixing ;
 fixing threatsfixing threatsfixing threatsfixing threats
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinModefixing , fixing )
 }
 fixing threatsfixing threatsfixing threats
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWritefixing fixing );// turn the LED on (HIGH is the voltage level)
   delay(3000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(3000);               // wait for a second
+  digitalWrite(fixing , fixing );    // turn the LED off by making the voltage LOW
+  delay(fixing dawdadsa);               // wait for a second
 }
