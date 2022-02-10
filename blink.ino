d2d016fd04ae87f2036325b2e33f1c7c1f3c1380
SALAVOZ <salik.abdulin@mail.ru>
2022-02-10 17:36:04 +0400

    fixing

--- blink.ino
+++ blink.ino
@@ -18,7 +18,11 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
+  delay(i fixed that);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  delay(i fixed that);               // wait for a second
 }
+fixing fixingfixingfixingfixingfixingfixingfixingfixingfixingfixingfixingfixing
+
+
+
