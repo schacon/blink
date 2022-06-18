f49871ac7d4fb6d35712a76c67460eb2795ee506
atah <habibiatah@gmail.com>
2022-06-18 10:13:59 -0700

    change off for 4 second and trailling space

--- blink.ino
+++ blink.ino
@@ -7,7 +7,7 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
+int led=13;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -20,5 +20,5 @@ void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(3000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(3000);               // wait for a second
+  delay(4000);               // wait for a second
 }
