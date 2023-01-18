49fadc96beffa4ba72cc56248fc0e3b67789da4d
Fil Lisaychuk <lisaychuk@lanit.ru>
2023-01-18 12:24:29 +0300

    тестирование перекрёстных ссылок 2

--- blink.ino
+++ blink.ino
@@ -22,4 +22,4 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(3000);               // wait three seconds
 }
-// тестирование перекрёстных ссылок
+// тестирование перекрёстных ссылок2
