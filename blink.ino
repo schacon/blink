af135d735015ce46f3f80d6c9079b22ab8074705
Chris <chris@vanextergem.eu>
2023-08-26 17:50:42 +0200

    updated comments with new values

--- blink.ino
+++ blink.ino
@@ -1,6 +1,6 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  Turns on an LED on for five seconds, then off for five seconds, repeatedly.
 
   This example code is in the public domain.
 */
