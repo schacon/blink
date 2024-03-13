fc913ee7e48650338220190fd06e0951fab38eff
Leung.Shyu <leung.shyu@gmail.com>
2024-03-13 21:59:44 +0900

    fix issue112

--- blink.ino
+++ blink.ino
@@ -21,4 +21,5 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(9000);               // wait for a second
+  git push origin slow-blink
 }
